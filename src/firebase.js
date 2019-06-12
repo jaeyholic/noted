81af4c3fbc5a77ca0819834e15ce74eefdbaa772
Felix Nana Yaw Yeboah-Jefferson <gabsco208309@hotmail.com>
2019-06-12 12:41:02 +0000

    dotenv

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCDcr1y5nDXu2yPhHMtcYbnH_go_-TKUY4',
-  authDomain: 'noted-a16b6.firebaseapp.com',
-  databaseURL: 'https://noted-a16b6.firebaseio.com',
-  projectId: 'noted-a16b6',
-  storageBucket: 'noted-a16b6.appspot.com',
-  messagingSenderId: '798675216744',
-  appId: '1:798675216744:web:4514be9676ca1341'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGE,
+  appId: process.env.REACT_APP_APP_ID
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
